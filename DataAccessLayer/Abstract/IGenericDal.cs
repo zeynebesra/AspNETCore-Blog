136ea38f6e355e3937d654513ba1fd047ba06707
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2023-05-07 21:56:35 +0300

    Generic Repository eklendi.

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -9,7 +9,7 @@ namespace DataAccessLayer.Abstract
 {
     public  interface IGenericDal<T> where T : class
     {
-        void Add(T t);
+        void Insert(T t);
         void Delete(T t);
         void Update(T t);
         List<T> GetList();
